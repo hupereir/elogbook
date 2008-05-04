fe6d019fee65fe9cc4df964c1e693f866ad2d824
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2008-05-04 15:29:58 +0000

    *** empty log message ***

--- src/elogbook.cpp
+++ src/elogbook.cpp
@@ -81,6 +81,7 @@ int main (int argc, char *argv[])
     if( Debug::level() ) XmlOptions::get().dump();
     
     // initialize main frame and run loop
+    Q_INIT_RESOURCE( basePixmaps );
     Q_INIT_RESOURCE( pixmaps );
     MainFrame main_frame(argc, argv);
     main_frame.initApplicationManager();
