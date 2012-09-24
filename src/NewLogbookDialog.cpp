cf22f1f52ac9886093eab01968c95bf98dd1371b
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2012-09-24 12:02:33 +0000

    code cleanup fixed layout for qftp gridftp and cvswatch (reduced margin with pathEditor) Added proper "abort" commands to gridwatch and gridftp

--- src/NewLogbookDialog.cpp
+++ src/NewLogbookDialog.cpp
@@ -2,85 +2,77 @@
 // $Id$
 
 /******************************************************************************
- *
- * Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>
- *
- * This is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * This software is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY;  without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.   See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * software; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA   02111-1307 USA
- *
- *
- *******************************************************************************/
-
-/*!
-   \file NewLogbookDialog.cpp
-   \brief new logbook popup dialog
-   \author Hugo Pereira
-   \version $Revision$
-   \date $Date$
-*/
-
-#include <QLabel>
-#include <QLayout>
+*
+* Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>
+*
+* This is free software; you can redistribute it and/or modify it under the
+* terms of the GNU General Public License as published by the Free Software
+* Foundation; either version 2 of the License, or (at your option) any later
+* version.
+*
+* This software is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY;  without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.   See the GNU General Public License
+* for more details.
+*
+* You should have received a copy of the GNU General Public License along with
+* software; if not, write to the Free Software Foundation, Inc., 59 Temple
+* Place, Suite 330, Boston, MA   02111-1307 USA
+*
+*
+*******************************************************************************/
 
 #include "Debug.h"
 #include "NewLogbookDialog.h"
 #include "QtUtil.h"
 
-
+#include <QtGui/QLabel>
+#include <QtGui/QLayout>
 
 //_______________________________________________________
 NewLogbookDialog::NewLogbookDialog( QWidget* parent ):
-  CustomDialog( parent )
+CustomDialog( parent )
 {
 
-  Debug::Throw( "NewLogbookDialog::NewLogbookDialog.\n" );
-  setWindowTitle( "New Logbook - Elogbook" );
-  QGridLayout *grid_layout( new QGridLayout() );
-  grid_layout->setMargin(0);
-  grid_layout->setSpacing(5);
-  mainLayout().addLayout( grid_layout, 0 );
+    Debug::Throw( "NewLogbookDialog::NewLogbookDialog.\n" );
+    setWindowTitle( "New Logbook - Elogbook" );
+    QGridLayout *gridLayout( new QGridLayout() );
+    gridLayout->setMargin(0);
+    gridLayout->setSpacing(5);
+    mainLayout().addLayout( gridLayout, 0 );
 
-  // title
-  grid_layout->addWidget( new QLabel( "Title: ", this ), 0, 0 );
-  grid_layout->addWidget( title_ = new AnimatedLineEditor( this ), 0, 1 );
-  title_->setToolTip( "Logbook title" );
+    // title
+    gridLayout->addWidget( new QLabel( "Title: ", this ), 0, 0 );
+    gridLayout->addWidget( title_ = new AnimatedLineEditor( this ), 0, 1 );
+    title_->setToolTip( "Logbook title" );
 
-  // logbook author
-  grid_layout->addWidget( new QLabel( "Author: ", this ), 1, 0 );
-  grid_layout->addWidget( author_ = new AnimatedLineEditor( this ), 1, 1 );
-  author_->setToolTip( "Logbook author." );
+    // logbook author
+    gridLayout->addWidget( new QLabel( "Author: ", this ), 1, 0 );
+    gridLayout->addWidget( author_ = new AnimatedLineEditor( this ), 1, 1 );
+    author_->setToolTip( "Logbook author." );
 
-  // attachment directory
-  grid_layout->addWidget( new QLabel( "File: ", this ), 2, 0 );
-  grid_layout->addWidget( file_ = new BrowsedLineEditor( this ), 2, 1 );
-  file_->setFileMode( QFileDialog::AnyFile );
-  file_->setToolTip( "File to which logbook entries are saved." );
+    // attachment directory
+    gridLayout->addWidget( new QLabel( "File: ", this ), 2, 0 );
+    gridLayout->addWidget( file_ = new BrowsedLineEditor( this ), 2, 1 );
+    file_->setFileMode( QFileDialog::AnyFile );
+    file_->setAcceptMode( QFileDialog::AcceptSave );
+    file_->setToolTip( "File to which logbook entries are saved." );
 
-  // attachment directory
-  grid_layout->addWidget( new QLabel( "Attachment directory: ", this ), 3, 0 );
-  grid_layout->addWidget( attachment_directory_ = new BrowsedLineEditor( this ), 3, 1 );
-  attachment_directory_->setFileMode( QFileDialog::DirectoryOnly );
-  attachment_directory_->setToolTip( "Default directory where attached files are stored (either copied or linked)." );
+    // attachment directory
+    gridLayout->addWidget( new QLabel( "Attachment directory: ", this ), 3, 0 );
+    gridLayout->addWidget( attachmentDirectory_ = new BrowsedLineEditor( this ), 3, 1 );
+    attachmentDirectory_->setFileMode( QFileDialog::DirectoryOnly );
+    attachmentDirectory_->setAcceptMode( QFileDialog::AcceptSave );
+    attachmentDirectory_->setToolTip( "Default directory where attached files are stored (either copied or linked)." );
 
-  grid_layout->setColumnStretch( 1, 1 );
+    gridLayout->setColumnStretch( 1, 1 );
 
-  // comments
-  mainLayout().addWidget( new QLabel( "Comments:", this ), 0 );
-  mainLayout().addWidget( comments_ = new TextEditor( this ), 1 );
-  comments_->setToolTip( "Logbook comments." );
-  Debug::Throw( "NewLogbookDialog::NewLogbookDialog - done.\n" );
+    // comments
+    mainLayout().addWidget( new QLabel( "Comments:", this ), 0 );
+    mainLayout().addWidget( comments_ = new TextEditor( this ), 1 );
+    comments_->setToolTip( "Logbook comments." );
+    Debug::Throw( "NewLogbookDialog::NewLogbookDialog - done.\n" );
 
-  adjustSize();
+    adjustSize();
 
 }
--- src/NewLogbookDialog.h
+++ src/NewLogbookDialog.h
@@ -4,35 +4,25 @@
 // $Id$
 
 /******************************************************************************
- *
- * Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>
- *
- * This is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * This software is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY;  without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.   See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * software; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA   02111-1307 USA
- *
- *
- *******************************************************************************/
-
-/*!
-   \file NewLogbookDialog.h
-   \brief new logbook popup dialog
-   \author Hugo Pereira
-   \version $Revision$
-   \date $Date$
-*/
-
-#include <QString>
+*
+* Copyright (C) 2002 Hugo PEREIRA <mailto: hugo.pereira@free.fr>
+*
+* This is free software; you can redistribute it and/or modify it under the
+* terms of the GNU General Public License as published by the Free Software
+* Foundation; either version 2 of the License, or (at your option) any later
+* version.
+*
+* This software is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY;  without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE.   See the GNU General Public License
+* for more details.
+*
+* You should have received a copy of the GNU General Public License along with
+* software; if not, write to the Free Software Foundation, Inc., 59 Temple
+* Place, Suite 330, Boston, MA   02111-1307 USA
+*
+*
+*******************************************************************************/
 
 #include "AnimatedLineEditor.h"
 #include "BrowsedLineEditor.h"
@@ -41,78 +31,80 @@
 #include "File.h"
 #include "Debug.h"
 
+#include <QtCore/QString>
+
 //! new attachment popup dialog
 class NewLogbookDialog: public CustomDialog
 {
 
-  public:
+    public:
 
-  //! constructor
-  NewLogbookDialog( QWidget* parent );
+    //! constructor
+    NewLogbookDialog( QWidget* parent );
 
-  //! destructor
-  virtual ~NewLogbookDialog( void )
-  { Debug::Throw( "NewLogbookDialog::~NewLogbookDialog\n" ); }
+    //! destructor
+    virtual ~NewLogbookDialog( void )
+    { Debug::Throw( "NewLogbookDialog::~NewLogbookDialog\n" ); }
 
-  //! title
-  void setTitle( const QString& title )
-  { title_->setText( title ); }
+    //! title
+    void setTitle( const QString& title )
+    { title_->setText( title ); }
 
-  //! title
-  QString title( void ) const
-  { return title_->text(); }
+    //! title
+    QString title( void ) const
+    { return title_->text(); }
 
-  //! author
-  void setAuthor( const QString& author )
-  { author_->setText( author ); }
+    //! author
+    void setAuthor( const QString& author )
+    { author_->setText( author ); }
 
-  //! filename
-  QString author( void ) const
-  { return author_->text(); }
+    //! filename
+    QString author( void ) const
+    { return author_->text(); }
 
-  //! filename
-  void setFile( const File& file )
-  { file_->setFile( file ); }
+    //! filename
+    void setFile( const File& file )
+    { file_->setFile( file ); }
 
-  //! filename
-  File file( void ) const
-  { return File( file_->editor().text() ).expand(); }
+    //! filename
+    File file( void ) const
+    { return File( file_->editor().text() ).expand(); }
 
-  //! attachment directory
-  void setAttachmentDirectory( const File& file )
-  { attachment_directory_->setFile( file ); }
+    //! attachment directory
+    void setAttachmentDirectory( const File& file )
+    { attachmentDirectory_->setFile( file ); }
 
-  //! attachment directory
-  File attachmentDirectory( void ) const
-  { return File( attachment_directory_->editor().text() ).expand(); }
+    //! attachment directory
+    File attachmentDirectory( void ) const
+    { return File( attachmentDirectory_->editor().text() ).expand(); }
 
-  //! comments
-  void setComments( const QString& comments )
-  {
-    Debug::Throw( "NewLogbookDialog::setComments.\n" );
-    comments_->setPlainText( comments );
-  }
+    //! comments
+    void setComments( const QString& comments )
+    {
+        Debug::Throw( "NewLogbookDialog::setComments.\n" );
+        comments_->setPlainText( comments );
+    }
 
-  //! comments
-  QString comments( void ) const
-  { return comments_->toPlainText(); }
+    //! comments
+    QString comments( void ) const
+    { return comments_->toPlainText(); }
 
-  private:
+    private:
 
-  //! title line edit
-  AnimatedLineEditor *title_;
+    //! title line edit
+    AnimatedLineEditor *title_;
 
-  //! author line edit
-  AnimatedLineEditor *author_;
+    //! author line edit
+    AnimatedLineEditor *author_;
 
-  //! filename browsed line editor
-  BrowsedLineEditor *file_;
+    //! filename browsed line editor
+    BrowsedLineEditor *file_;
 
-  //! destination directory browsed line edti
-  BrowsedLineEditor *attachment_directory_;
+    //! destination directory browsed line edti
+    BrowsedLineEditor *attachmentDirectory_;
 
-  //! comments
-  TextEditor *comments_;
+    //! comments
+    TextEditor *comments_;
 
 };
 
