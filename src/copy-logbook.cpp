22447e13d0547de2c3e69ae26454e19c83f5e2a1
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2012-10-13 18:42:49 +0000

    code cleanup.

--- src/copy-logbook.cpp
+++ src/copy-logbook.cpp
@@ -21,29 +21,17 @@
 *
 *******************************************************************************/
 
-/*!
-  \file copy_logbook.cpp
-  \brief comand line copy of a logbook
-  \author  Hugo Pereira
-  \version $Revision$
-  \date    $Date$
-*/
-
-
-#include <QCoreApplication>
-#include <QString>
-
-
-#include <signal.h>
-#include <unistd.h>
-
 #include "Debug.h"
 #include "DefaultOptions.h"
 #include "Logbook.h"
 #include "XmlOptions.h"
 #include "Util.h"
 
+#include <QtCore/QCoreApplication>
+#include <QtCore/QString>
 
+#include <signal.h>
+#include <unistd.h>
 
 //_______________________________
 //! to handle keyboard interruptions
@@ -54,97 +42,97 @@ void interrupt( int sig );
 int main (int argc, char *argv[])
 {
 
-  // Ensure proper cleaning at exit
-  signal(SIGINT,  interrupt);
-  signal(SIGTERM, interrupt);
+    // Ensure proper cleaning at exit
+    signal(SIGINT,  interrupt);
+    signal(SIGTERM, interrupt);
 
-  // read argument
-  if( argc < 3 )
-  {
-    Debug::Throw(0) << "usage: copy_logbook <input file> <output file>" << endl;
-    return 0;
-  }
-
-  // load argument
-  QString input( argv[1] );
-  QString output( argv[2] );
-
-  // load options
-  QString user( Util::user( ) );
-  QString host( Util::host() );
-  XmlOptions::get().set( "USER", Option( user+"@"+host, Option::None ) );
-
-  // install default options
-  installDefaultOptions();
-
-  // load user resource file
-  QString rcfile = Util::env( "HOME", "." ) + "/.eLogbookrc";
-  XmlOptions::read( rcfile );
-
-  // force debug level to 0
-  XmlOptions::get().set<int>("DEBUG_LEVEL", 0 );
-
-  // set debug level
-  int debug_level( XmlOptions::get().get<int>( "DEBUG_LEVEL" ) );
-  Debug::setLevel( debug_level );
-  if( debug_level ) XmlOptions::get().print();
-
-  // the core application is needed to have locale, fonts, etc. set properly, notably for QSting
-  // not having it might result in lost accents and special characters.
-  QCoreApplication application( argc, argv );
-
-  // try open input logbook
-  Debug::Throw(0) << "copy_logbook::main - reading from: " << input << endl;
-  Logbook logbook;
-  logbook.setFile( File(input).expand() );
-  logbook.read();
-
-  // debug
-  Debug::Throw(0) << "copy_logbook::main - number of files: " << logbook.children().size() << endl;
-  Debug::Throw(0) << "copy_logbook::main - number of entries: " << logbook.entries().size() << endl;
-
-  // perform copy
-  Debug::Throw(0) << "copy_logbook::main - writing to: " << output << endl;
-  logbook.setFile( File( output ).expand() );
-  logbook.setModifiedRecursive( true );
-
-  // check logbook filename is writable
-  File fullname = File( logbook.file() ).expand();
-  if( fullname.exists() ) {
-
-    // check file is not a directory
-    if( fullname.isDirectory() )
+    // read argument
+    if( argc < 3 )
     {
-      Debug::Throw(0) << "copy_logbook::main - selected file is a directory. <Save Logbook> canceled." << endl;
-      return 0;
+        Debug::Throw(0) << "usage: copy-logbook <input file> <output file>" << endl;
+        return 0;
     }
 
-    // check file is writable
-    if( !fullname.isWritable() ) {
-      Debug::Throw(0) << "copy_logbook::main - selected file is not writable. <Save Logbook> canceled." << endl;
-      return 0;
+    // load argument
+    QString input( argv[1] );
+    QString output( argv[2] );
+
+    // load options
+    QString user( Util::user( ) );
+    QString host( Util::host() );
+    XmlOptions::get().set( "USER", Option( user+"@"+host, Option::None ) );
+
+    // install default options
+    installDefaultOptions();
+
+    // load user resource file
+    QString rcfile = Util::env( "HOME", "." ) + "/.eLogbookrc";
+    XmlOptions::read( rcfile );
+
+    // force debug level to 0
+    XmlOptions::get().set<int>("DEBUG_LEVEL", 0 );
+
+    // set debug level
+    int debug_level( XmlOptions::get().get<int>( "DEBUG_LEVEL" ) );
+    Debug::setLevel( debug_level );
+    if( debug_level ) XmlOptions::get().print();
+
+    // the core application is needed to have locale, fonts, etc. set properly, notably for QSting
+    // not having it might result in lost accents and special characters.
+    QCoreApplication application( argc, argv );
+
+    // try open input logbook
+    Debug::Throw(0) << "copy-logbook - reading from: " << input << endl;
+    Logbook logbook;
+    logbook.setFile( File(input).expand() );
+    logbook.read();
+
+    // debug
+    Debug::Throw(0) << "copy-logbook - number of files: " << logbook.children().size() << endl;
+    Debug::Throw(0) << "copy-logbook - number of entries: " << logbook.entries().size() << endl;
+
+    // perform copy
+    Debug::Throw(0) << "copy-logbook - writing to: " << output << endl;
+    logbook.setFile( File( output ).expand() );
+    logbook.setModifiedRecursive( true );
+
+    // check logbook filename is writable
+    File fullname = File( logbook.file() ).expand();
+    if( fullname.exists() ) {
+
+        // check file is not a directory
+        if( fullname.isDirectory() )
+        {
+            Debug::Throw(0) << "copy-logbook - selected file is a directory. <Save Logbook> canceled." << endl;
+            return 0;
+        }
+
+        // check file is writable
+        if( !fullname.isWritable() ) {
+            Debug::Throw(0) << "copy-logbook - selected file is not writable. <Save Logbook> canceled." << endl;
+            return 0;
+        }
+
+    } else {
+
+        File path( fullname.path() );
+        if( !path.isDirectory() ) {
+            Debug::Throw(0) << "copy-logbook - selected path is not valid. <Save Logbook> canceled." << endl;
+            return 0;
+        }
+
     }
 
-  } else {
+    // copy logbook to ouput
+    if( !logbook.write() )
+    { Debug::Throw(0) << "copy-logbook - error writing to file " << output << endl; }
 
-    File path( fullname.path() );
-    if( !path.isDirectory() ) {
-      Debug::Throw(0) << "copy_logbook::main - selected path is not valid. <Save Logbook> canceled." << endl;
-      return 0;
-    }
-
-  }
-
-  // copy logbook to ouput
-  if( !logbook.write() )
-  { Debug::Throw(0) << "copy_logbook::main - error writing to file " << output << endl; }
-
-  return 0;
+    return 0;
 }
 
 //_____________________________________________
 void interrupt( int sig )
 {
-  Debug::Throw() << "copy_logbook::interrupt - Recieved signal " << sig << endl;
-  exit(0);
+    Debug::Throw() << "copy-logbook::interrupt - Recieved signal " << sig << endl;
+    exit(0);
 }
--- src/synchronize-logbook.cpp
+++ src/synchronize-logbook.cpp
@@ -21,28 +21,16 @@
 *
 *******************************************************************************/
 
-/*!
-  \file synchronize_logbook.cpp
-  \brief comand line synchronization of a logbook
-  \author  Hugo Pereira
-  \version $Revision$
-  \date    $Date$
-*/
-
-#include <QCoreApplication>
-#include <QString>
-
-
-#include <signal.h>
-#include <unistd.h>
-
 #include "Debug.h"
 #include "DefaultOptions.h"
 #include "Logbook.h"
 #include "Options.h"
 #include "Util.h"
 
-
+#include <QtCore/QCoreApplication>
+#include <QtCore/QString>
+#include <signal.h>
+#include <unistd.h>
 
 //_______________________________
 //! to handle keyboard interruptions
@@ -52,102 +40,102 @@ void interrupt( int sig );
 //! main function
 int main (int argc, char *argv[])
 {
-  // Ensure proper cleaning at exit
-  signal(SIGINT,  interrupt);
-  signal(SIGTERM, interrupt);
+    // Ensure proper cleaning at exit
+    signal(SIGINT,  interrupt);
+    signal(SIGTERM, interrupt);
 
-  // read argument
-  if( argc < 3 )
-  {
-    Debug::Throw(0) << "usage: synchronize_logbook <local file> <remote file>" << endl;
+    // read argument
+    if( argc < 3 )
+    {
+        Debug::Throw(0) << "usage: synchronize-logbook <local file> <remote file>" << endl;
+        return 0;
+    }
+
+    // load argument
+    QString local( argv[1] );
+    QString remote( argv[2] );
+
+    // load options
+    QString user( Util::user( ) );
+    QString host( Util::host() );
+    XmlOptions::get().set( "USER", Option( user+"@"+host, Option::None ) );
+
+    // install default options
+    installDefaultOptions();
+
+    // load user resource file
+    QString rcfile = Util::env( "HOME", "." ) + "/.eLogbookrc";
+    XmlOptions::read( rcfile );
+
+    // force debug level to 0
+    XmlOptions::get().set<int>("DEBUG_LEVEL", 0 );
+
+    // set debug level
+    int debug_level( XmlOptions::get().get<int>( "DEBUG_LEVEL" ) );
+    Debug::setLevel( debug_level );
+    if( debug_level ) XmlOptions::get().print();
+
+    // the core application is needed to have locale, fonts, etc. set properly, notably for QSting
+    // not having it might result in lost accents and special characters.
+    QCoreApplication application( argc, argv );
+
+    // try open local_logbook
+    Debug::Throw(0) << "synchronize-logbook - reading local logbook from: " << local << endl;
+    Logbook local_logbook;
+    local_logbook.setFile( File( local ).expand() );
+    if( !local_logbook.read() )
+    {
+        Debug::Throw(0) << "synchronize-logbook - error reading local logbook" << endl;
+        return 0;
+    }
+
+    // debug
+    Debug::Throw(0) << "synchronize-logbook - number of local files: " << local_logbook.children().size() << endl;
+    Debug::Throw(0) << "synchronize-logbook - number of local entries: " << local_logbook.entries().size() << endl;
+
+    // try open local_logbook
+    Debug::Throw(0) << "synchronize-logbook - reading remote logbook from: " << remote << endl;
+    Logbook remote_logbook;
+    remote_logbook.setFile( File( remote ).expand() );
+    if( !remote_logbook.read() )
+    {
+        Debug::Throw(0) << "synchronize-logbook - error reading remote logbook" << endl;
+        return 0;
+    }
+
+    // debug
+    Debug::Throw(0) << "synchronize-logbook - number of remote files: " << remote_logbook.children().size() << endl;
+    Debug::Throw(0) << "synchronize-logbook - number of remote entries: " << remote_logbook.entries().size() << endl;
+
+    Debug::Throw(0) << "synchronize-logbook - updating local from remote" << endl;
+    int n_duplicated = local_logbook.synchronize( remote_logbook ).size();
+
+    Debug::Throw(0) << "synchronize-logbook - number of duplicated entries: " << n_duplicated << endl;
+
+    if( !local_logbook.write() )
+    {
+        Debug::Throw(0) << "synchronize-logbook - error writing local logbook" << endl;
+        return 0;
+    }
+
+    Debug::Throw(0) << "synchronize-logbook - updating remote from local" << endl;
+    n_duplicated = remote_logbook.synchronize( local_logbook ).size();
+
+    Debug::Throw(0) << "synchronize-logbook - number of duplicated entries: " << n_duplicated << endl;
+
+    if( !remote_logbook.write() )
+    {
+        Debug::Throw(0) << "error writting to remote logbook" << endl;
+        return 0;
+    }
+
     return 0;
-  }
-
-  // load argument
-  QString local( argv[1] );
-  QString remote( argv[2] );
-
-  // load options
-  QString user( Util::user( ) );
-  QString host( Util::host() );
-  XmlOptions::get().set( "USER", Option( user+"@"+host, Option::None ) );
-
-  // install default options
-  installDefaultOptions();
-
-  // load user resource file
-  QString rcfile = Util::env( "HOME", "." ) + "/.eLogbookrc";
-  XmlOptions::read( rcfile );
-
-  // force debug level to 0
-  XmlOptions::get().set<int>("DEBUG_LEVEL", 0 );
-
-  // set debug level
-  int debug_level( XmlOptions::get().get<int>( "DEBUG_LEVEL" ) );
-  Debug::setLevel( debug_level );
-  if( debug_level ) XmlOptions::get().print();
-
-  // the core application is needed to have locale, fonts, etc. set properly, notably for QSting
-  // not having it might result in lost accents and special characters.
-  QCoreApplication application( argc, argv );
-
-  // try open local_logbook
-  Debug::Throw(0) << "synchronize_logbook - reading local logbook from: " << local << endl;
-  Logbook local_logbook;
-  local_logbook.setFile( File( local ).expand() );
-  if( !local_logbook.read() )
-  {
-    Debug::Throw(0) << "synchronize_logbook - error reading local logbook" << endl;
-    return 0;
-  }
-
-  // debug
-  Debug::Throw(0) << "synchronize_logbook - number of local files: " << local_logbook.children().size() << endl;
-  Debug::Throw(0) << "synchronize_logbook - number of local entries: " << local_logbook.entries().size() << endl;
-
-  // try open local_logbook
-  Debug::Throw(0) << "synchronize_logbook - reading remote logbook from: " << remote << endl;
-  Logbook remote_logbook;
-  remote_logbook.setFile( File( remote ).expand() );
-  if( !remote_logbook.read() )
-  {
-    Debug::Throw(0) << "synchronize_logbook - error reading remote logbook" << endl;
-    return 0;
-  }
-
-  // debug
-  Debug::Throw(0) << "synchronize_logbook - number of remote files: " << remote_logbook.children().size() << endl;
-  Debug::Throw(0) << "synchronize_logbook - number of remote entries: " << remote_logbook.entries().size() << endl;
-
-  Debug::Throw(0) << "synchronize_logbook - updating local from remote" << endl;
-  int n_duplicated = local_logbook.synchronize( remote_logbook ).size();
-
-  Debug::Throw(0) << "synchronize_logbook - number of duplicated entries: " << n_duplicated << endl;
-
-  if( !local_logbook.write() )
-  {
-    Debug::Throw(0) << "synchronize_logbook - error writing local logbook" << endl;
-    return 0;
-  }
-
-  Debug::Throw(0) << "synchronize_logbook - updating remote from local" << endl;
-  n_duplicated = remote_logbook.synchronize( local_logbook ).size();
-
-  Debug::Throw(0) << "synchronize_logbook - number of duplicated entries: " << n_duplicated << endl;
-
-  if( !remote_logbook.write() )
-  {
-    Debug::Throw(0) << "error writting to remote logbook" << endl;
-    return 0;
-  }
-
-  return 0;
 
 }
 
 //_____________________________________________
 void interrupt( int sig )
 {
-  Debug::Throw() << "interrupt - Recieved signal " << sig << endl;
-  exit(0);
+    Debug::Throw() << "interrupt - Recieved signal " << sig << endl;
+    exit(0);
 }
