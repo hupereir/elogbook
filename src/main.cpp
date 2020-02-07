e8e80a475e49352de893588c31f2a10fa7a27ba9
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2020-02-06 22:08:27 -0700

    removed unnecessary QINIT_RESOURCE

--- src/main.cpp
+++ src/main.cpp
@@ -56,7 +56,6 @@ int main (int argc, char *argv[])
     if( Debug::level() ) Debug::Throw() << XmlOptions::get() << endl;
 
     // resources
-    Q_INIT_RESOURCE( basePixmaps );
     Q_INIT_RESOURCE( pixmaps );
 
     // create Application
